a579b2480f6d70463259a3166d614135d6547265
ArlatPS <przemekx10@gmail.com>
2023-03-21 23:28:40 +0100

    new test data

--- dprep.py
+++ dprep.py
@@ -21,28 +21,66 @@ for i in range(my_data.shape[1]):
 # print(zeros)
 # print(ones)
 
-# take 10% + 1 of data for testing
-zeros_tst = zeros[0:20]
-ones_tst = ones[12:16]
+# take 15% + 1 of data for testing randomly
+# zeros
+N_zeros = 190
+N_zeros_to_test = 28
+zeros_tst = np.zeros((N_zeros_to_test, 120))
+ZT_count = -1;
+new_zeros = np.zeros((N_zeros - N_zeros_to_test, 120))
+NZ_count = -1
+idx = np.argsort(np.random.random(N_zeros))
+for i in range(N_zeros):
+    if i in idx[0:N_zeros_to_test]: 
+        ZT_count += 1
+        zeros_tst[ZT_count] = zeros[i]
+    else: 
+        NZ_count +=1
+        new_zeros[NZ_count] = zeros[i]
+
+N_ones = 31;
+N_ones_to_test = 7;
+ones_tst = np.zeros((N_ones_to_test, 120))
+ZT_count = -1;
+new_ones = np.zeros((N_ones - N_ones_to_test, 120))
+NZ_count = -1
+idx = np.argsort(np.random.random(N_ones))
+for i in range(N_ones):
+    if i in idx[0:N_ones_to_test]: 
+        ZT_count += 1
+        ones_tst[ZT_count] = ones[i]
+    else: 
+        NZ_count +=1
+        new_ones[NZ_count] = ones[i]
+
+
 
 # data for learning shuffled 
-data = np.concatenate((zeros[20:], ones[0:12], ones[16:]))
-labels = np.concatenate((np.zeros(170), np.ones(27)))
+data = np.concatenate((new_zeros, new_ones))
+labels = np.concatenate((np.zeros(N_zeros - N_zeros_to_test), np.ones(N_ones - N_ones_to_test)))
 
 # new indexes
-idx = np.argsort(np.random.random(197));
+idx = np.argsort(np.random.random(N_zeros - N_zeros_to_test + N_ones - N_ones_to_test ));
 data = data[idx]
 labels = labels[idx]
 
-# testing data
-data_tst = np.concatenate((zeros_tst, ones_tst))
-labels_tst = np.concatenate((np.zeros(20), np.ones(4)))
+## more balanced validation data
+
+
+
+# # testing data
+# data_tst = np.concatenate((zeros_tst, ones_tst))
+# labels_tst = np.concatenate((np.zeros(20), np.ones(4)))
+
+print(data.shape)
+print(labels.shape)
+print(zeros_tst.shape)
+print(ones_tst.shape)
 
 
 np.save("x_trn.npy", data)
 np.save("y_trn.npy", labels)
-np.save("x_tst.npy", data_tst)
-np.save("y_tst.npy", labels_tst)
+
 np.save("zeros_tst.npy", zeros_tst)
 np.save("ones_tst.npy", ones_tst)
 
--- neural_network.py
+++ neural_network.py
@@ -6,10 +6,13 @@ import numpy as np
 # load data
 x_trn = np.load('x_trn.npy')
 y_trn = np.load('y_trn.npy')
-x_test = np.load('x_tst.npy')
-y_test = np.load('y_tst.npy')
+# x_test = np.load('x_tst.npy')
+# y_test = np.load('y_tst.npy')
+zeros_tst = np.load('zeros_tst.npy')
+ones_tst = np.load('ones_tst.npy')
 
-# adding 5% validation data
+
+# adding 5% validation data (its already shuffled)
 x_val = x_trn[0:10]
 x_trn = x_trn[10:]
 y_val = y_trn[0:10]
@@ -33,8 +36,12 @@ print(model.summary())
 
 history = model.fit(x_trn, y_trn, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_val,y_val))
 
-score = model.evaluate(x_test,y_test, verbose=0)
+score = model.evaluate(zeros_tst ,np.zeros(28), verbose=0)
+score2 = model.evaluate(ones_tst ,np.ones(7), verbose=0)
+
 
 print('Test loss:     ', score[0])
 print('Test accuracy: ', score[1])
+print('Test loss:     ', score2[0])
+print('Test accuracy: ', score2[1])
 model.save('mea_NN_model.h5')
